c2a0cdc4eae71fd224768d2a43604ea47090c3f3
Avery Lin <tclin100@gmail.com>
2021-12-24 11:53:22 -0500

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :parkdetails, only: [:index, :show, :create, :update, :destroy]
     resources :packinglists, only: [:index, :show, :create, :update, :destroy]
     resources :users, only: [:index, :create, :destroy]
+    resource :parks
     # Routing logic: fallback requests for React Router.
     # Leave this here to help deploy your app later!
     post '/login', to: "sessions#create"
@@ -13,7 +14,6 @@ Rails.application.routes.draw do
     post '/signup', to: "users#create"
     get "/me", to: "users#show"
     get '/parks', to: "parks#get_parks"
-    resource :parks
     get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   end
 
